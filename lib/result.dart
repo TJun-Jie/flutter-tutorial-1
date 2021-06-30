9fc5367d1d76506ec22584634808abbde5098f57
TJun-Jie <tjunjie2000@gmail.com>
2021-06-30 19:40:05 +0800

    completed first flutter tutorial

--- lib/result.dart
+++ lib/result.dart
@@ -3,7 +3,9 @@ import 'package:flutter/material.dart';
 class Result extends StatelessWidget {
   final int resultScore;
 
-  const Result({Key key, this.resultScore}) : super(key: key);
+  final VoidCallback resetQuiz;
+
+  const Result({Key key, this.resultScore, this.resetQuiz}) : super(key: key);
 
   String get resultPhrase {
     var resultText = 'You did it!';
@@ -21,9 +23,18 @@ class Result extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-        child: Text(
-      resultPhrase,
-      style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        child: Column(
+      children: [
+        Text(
+          resultPhrase,
+          style: TextStyle(fontSize: 36, fontWeight: FontWeight.bold),
+        ),
+        FlatButton(
+          onPressed: resetQuiz,
+          child: Text('Reset'),
+          textColor: Colors.blue,
+        )
+      ],
     ));
   }
 }
